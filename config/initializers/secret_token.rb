fd481dd21a3768f6d51377946654f8be6a3ffd6c
janeliu <janeliu94@gmail.com>
2013-08-21 15:47:58 -0700

    Initialize database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseExchange::Application.config.secret_token = '1605cc289e2744ee930347c0edfa1e822e74936db6b6090ae605cf07b569ce1fc55b992237437a8f4a87127dfbd05bcd9e2ac9899c53e6acbab62b8113c2e9f1'
